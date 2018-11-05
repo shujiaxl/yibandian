ee6d9734dbdcfd2c61d2dd0aab96a621921deba9
shujiaxl <350619965@qq.com>
2018-11-05 21:03:29 +0800

    哈哈

---
+++ src/Demo01/Test01.java
@@ -0,0 +1,18 @@
+package Demo01;
+
+import java.io.File;
+import java.util.Random;
+
+public class Test01 {
+    public static void main(String[] args) {
+//        System.out.println(Math.random());
+//        System.out.println(Math.random()*10+10);
+//        System.out.println(Math.sqrt(16));
+        File  file = new File("e:\\demo\test.txt");
+        System.out.println(file);
+
+    }
+
+
+
+}
